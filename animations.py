581a13145a9b89c292d5f848f878d4f33ea0f305
BrunoHenryCDMal <brunohenriques83@gmail.com>
2015-11-19 13:55:07 +0100

    better version of mass_gr animation

---
+++ animations.py
@@ -0,0 +1,33 @@
+'''
+basic_animation()
+'''
+
+from scipy.ndimage import zoom 
+
+
+def basic_animation():
+    
+    fig = plt.figure()
+    ax = plt.axes(xlim=(0, 2), ylim=(-2, 2))
+    line, = ax.plot([], [], lw=2)    
+   
+    # initialization function: plot the background of each frame
+    def init():
+        line.set_data([], [])
+        return line,
+
+    # animation function.  This is called sequentially
+    def animate(i):
+        x = np.linspace(0, 2, 1000)
+        y = np.sin(2 * np.pi * (x - 0.01 * i))
+        line.set_data(x, y)
+        return line,
+
+    # call the animator.  blit=True means only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=200, interval=20, blit=True)
+    
+    anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+    plt.show()
+    
+#end basic_animation
